9b2578d2f3fb292158fdbcfd6145498c3a530eb8
Jeong seulho <wjdtmfgh@gmail.com>
2023-05-17 21:30:30 +0900

    create accounts, community models

--- final-pjt-back/community/models.py
+++ final-pjt-back/community/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # 게시글을 작성한 사용자
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='articles')
+    # 게시글을 좋아요한 사용자
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # 댓글 작성자
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+
+    # 댓글이 종속된 글
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,  related_name='comments')
