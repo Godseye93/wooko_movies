7efd16eb2101d7f5e22cc1348aa45116f4e80cbb
wook <wook7416@naver.com>
2023-05-18 03:14:47 +0900

    더미데이터 수정, views.py, models.py, 직렬화, urls.py 수정

--- final-pjt-back/movies/serializers.py
+++ final-pjt-back/movies/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
-from .models import Movie
+from .models import Genre, Movie
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['id', 'name']
 
 
 class MovieSerializer(serializers.ModelSerializer):
+    genres = GenreSerializer(many=True, read_only=True)
+
     class Meta:
         model = Movie
-        fields = '__all__'
+        fields = ['id', 'title', 'release_date', 'popularity', 'vote_average',
+                  'overview', 'poster_path', 'backdrop_path', 'genres']
