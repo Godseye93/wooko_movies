64df61f10755d3e398106582f438c347365c494b
Jeong seulho <wjdtmfgh@gmail.com>
2023-05-22 14:17:11 +0900

    Design: Home

--- final-pjt-front/finalfront/src/components/MovieList.vue
+++ final-pjt-front/finalfront/src/components/MovieList.vue
@@ -49,6 +49,26 @@
               </div>
             </div>
           </div>
+          <button
+            class="carousel-control-prev"
+            type="button"
+            :data-bs-target="`#${slideId}`"
+            data-bs-slide="prev"
+            style="left: -130px"
+          >
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button
+            class="carousel-control-next"
+            type="button"
+            :data-bs-target="`#${slideId}`"
+            data-bs-slide="next"
+            style="right: -130px"
+          >
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
       </div>
     </div>
